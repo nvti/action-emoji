93aa2e82cf62eedce673097acdd62cd215de9769
Viet-Tien Ngoc <tiena2cva@gmail.com>
2020-12-15 17:35:24 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8-alpine
 
 RUN apk --update --no-cache add git bash
+RUN pip install gitpython
 
 COPY "emoji.py" "/emoji.py"
 
--- emoji.py
+++ emoji.py
@@ -1,6 +1,10 @@
 import json
 import os
+import git
 
 with open(os.environ["GITHUB_EVENT_PATH"]) as json_file:
     data = json.load(json_file)
     print(data)
+
+g = git.cmd.Git()
+print(g.log())
