802bec2c7ff3265edd764ea704874348ec2c41ad
Viet-Tien Ngoc <tiena2cva@gmail.com>
2020-12-16 13:18:07 +0700

    update

--- emoji.py
+++ emoji.py
@@ -7,4 +7,10 @@ with open(os.environ["GITHUB_EVENT_PATH"]) as json_file:
     print(data)
 
 g = Github(os.environ["INPUT_GITHUB_TOKEN"])
-print(g)
+repo = g.get_user().get_repo(data['repository']['full_name'])
+
+pull = repo.get_pull(data['pull_request']['number'])
+
+pull.title += ":emoji:"
+
+pull.update()
