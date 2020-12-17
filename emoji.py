548da6126e10dc4f535266e3bb2677ae2ae3c8f5
Viet-Tien Ngoc <tiena2cva@gmail.com>
2020-12-17 17:38:12 +0700

    Tao board

--- emoji.py
+++ emoji.py
@@ -7,7 +7,7 @@ with open(os.environ["GITHUB_EVENT_PATH"]) as json_file:
     print(data)
 
 g = Github(os.environ["INPUT_GITHUB_TOKEN"])
-repo = g.get_user().get_repo(data['repository']['full_name'])
+repo = g.get_repo(data['repository']['full_name'])
 
 pull = repo.get_pull(data['pull_request']['number'])
 
