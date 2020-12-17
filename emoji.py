48620f0474d3ec2205671f8ce4a099ca35171c91
Viet-Tien Ngoc <tiena2cva@gmail.com>
2020-12-17 17:47:37 +0700

    Tao board

--- emoji.py
+++ emoji.py
@@ -10,7 +10,4 @@ g = Github(os.environ["INPUT_GITHUB_TOKEN"])
 repo = g.get_repo(data['repository']['full_name'])
 
 pull = repo.get_pull(data['pull_request']['number'])
-
-pull.title += ":emoji:"
-
-pull.update()
+pull.edit(title=":emoji: " + pull.title)
