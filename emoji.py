e659f1eb4a24c9ffea7adf25aa0a45e55e38ddc2
Viet-Tien Ngoc <tiena2cva@gmail.com>
2020-12-17 18:02:07 +0700

    update

--- emoji.py
+++ emoji.py
@@ -1,6 +1,14 @@
 import json
 import os
+import re
 from github import Github
+
+
+def check_emoji(title):
+    result = re.search("^:[a-z0-9_]+:", title)
+    print(result)
+    return result != None
+
 
 with open(os.environ["GITHUB_EVENT_PATH"]) as json_file:
     data = json.load(json_file)
@@ -10,4 +18,6 @@ g = Github(os.environ["INPUT_GITHUB_TOKEN"])
 repo = g.get_repo(data['repository']['full_name'])
 
 pull = repo.get_pull(data['pull_request']['number'])
-pull.edit(title=":emoji: " + pull.title)
+
+if not check_emoji(pull.title):
+    pull.edit(title=":jack_o_lantern: " + pull.title.trim())
