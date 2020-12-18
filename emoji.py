044e31e7fabf0f7a9dbc6d0ca4285a2150e706a7
Viet-Tien <tiena2cva@gmail.com>
2020-12-18 13:09:08 +0700

    :grinning: Add user config file (#6)
    * :sparkles: add user config file
    
    * :memo: Update readme

--- emoji.py
+++ emoji.py
@@ -11,19 +11,55 @@ def check_emoji(title):
     return result != None
 
 
-def load_emoji():
-    emojis = []
-    with open("/emoji.yaml", 'r') as f:
+def load_config():
+    if os.path.isfile("./.github/emoji.yml"):
+        config_file = "./.github/emoji.yml"
+    elif os.path.isfile("./.github/emoji.yaml"):
+        config_file = "./.github/emoji.yaml"
+    else:
+        return {"all": True}
+
+    config = {
+        "all": False,
+        "lists": [],
+        "customs": []
+    }
+
+    with open(config_file, 'r') as f:
         try:
             data = yaml.safe_load(f)
-            for _, value in data.items():
-                emojis.extend(value)
-
-            return emojis
+            for key in config:
+                if key in data:
+                    config[key] = data[key]
 
         except yaml.YAMLError as exc:
             print(exc)
-            return ["kissing_heart"]
+
+        return config
+
+
+def load_emoji():
+    config = load_config()
+    emojis = config['customs']
+    with open("/emoji.yaml", 'r') as f:
+        try:
+            data = yaml.safe_load(f)
+            if config['all']:
+                topics = data.keys()
+            else:
+                topics = config['lists']
+
+            for key, value in data.items():
+                if key in topics:
+                    emojis.extend(value)
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    if len(emojis) > 0:
+        return emojis
+    else:
+        return ["kissing_heart"]
 
 
 def get_emoji(title, emojis):
@@ -38,10 +74,11 @@ repo = g.get_repo(data['repository']['full_name'])
 
 pull = repo.get_pull(data['pull_request']['number'])
 
-emojis = load_emoji()
-print(f"Loaded {len(emojis)} emoji")
 
 if not check_emoji(pull.title):
+    emojis = load_emoji()
+    print(f"Loaded {len(emojis)} emoji")
+
     emoji = get_emoji(pull.title, emojis)
     print(f"Add emoji :{emoji}: to title")
     pull.edit(title=f":{emoji}: {pull.title.strip()}")
