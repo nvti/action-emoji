631dfc5fd90221c6a4bb502acb44a19cf9be3afd
Viet-Tien <tiena2cva@gmail.com>
2020-12-18 11:45:05 +0700

    :dancer: Add list of emoji to choose (#4)

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.8-alpine
 
 RUN apk --update --no-cache add git bash
-RUN pip install gitpython PyGithub
+RUN pip install PyGithub PyYAML
 
 COPY "emoji.py" "/emoji.py"
+COPY "emoji.yaml" "/emoji.yaml"
 
 ENTRYPOINT ["python", "emoji.py"]
--- emoji.py
+++ emoji.py
@@ -1,12 +1,33 @@
 import json
 import os
 import re
+import random
+import yaml
 from github import Github
 
 
 def check_emoji(title):
-    result = re.search("^:[a-z0-9_]+:", title)
+    result = re.search("^:[a-z0-9_\+\-]+:", title)
     return result != None
+
+
+def load_emoji():
+    emojis = []
+    with open("/emoji.yaml", 'r') as f:
+        try:
+            data = yaml.safe_load(f)
+            for _, value in data.items():
+                emojis.extend(value)
+
+            return emojis
+
+        except yaml.YAMLError as exc:
+            print(exc)
+            return ["kissing_heart"]
+
+
+def get_emoji(title, emojis):
+    return random.choice(emojis)
 
 
 with open(os.environ["GITHUB_EVENT_PATH"]) as json_file:
@@ -17,5 +38,10 @@ repo = g.get_repo(data['repository']['full_name'])
 
 pull = repo.get_pull(data['pull_request']['number'])
 
+emojis = load_emoji()
+print(f"Loaded {len(emojis)} emoji")
+
 if not check_emoji(pull.title):
-    pull.edit(title=":jack_o_lantern: " + pull.title.strip())
+    emoji = get_emoji(pull.title, emojis)
+    print(f"Add emoji :{emoji}: to title")
+    pull.edit(title=f":{emoji}: {pull.title.strip()}")
